655a5657df2ad90fc3c3c60e56031142455bcd10
PhucAnDev <phucan0147@gmail.com>
2026-03-07 20:52:40 +0700

    [FIX] config deploy in vercel

--- src/App.tsx
+++ src/App.tsx
@@ -227,7 +227,6 @@ export default function App() {
         style={{ fontFamily: "'Montserrat', sans-serif" }}
       >
         <AdminDashboard onNavigate={handleNavigate} />
-        <ChatBot />
       </div>
     );
   }
